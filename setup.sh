17830fb7bc25cead7bfd7afa496d79ea54649105
gallosombi2-cloud <gallosombi2@gmail.com>
2026-02-21 17:18:15 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-echo "Instalando dependencias..."
-sudo apt update && sudo apt install xterm zenity aircrack-ng -y
+# Instalador de dependencias para WiFi-Killer-Scan
+echo "------------------------------------------"
+echo "🛠️ Instalando herramientas de Seguridad..."
+echo "------------------------------------------"
+
+sudo apt update
+sudo apt install xterm zenity aircrack-ng -y
+
+# Dar permisos de ejecución al script principal
 chmod +x killer_scan.sh
-echo "Listo. Ejecuta con: sudo ./killer_scan.sh"
+
+echo ""
+echo "✅ Instalación completada con éxito."
+echo "🚀 Ejecuta la herramienta con: sudo ./killer_scan.sh"
